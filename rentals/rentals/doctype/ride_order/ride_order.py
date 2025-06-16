6f2357352cecd6da8c315175a8fbd86f1503b1eb
YOUSEF <yousif0180@gmail.com>
2025-06-16 17:00:41 +0300

    init

---
+++ rentals/rentals/doctype/ride_order/ride_order.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, yousef and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class RideOrder(Document):
+	pass
