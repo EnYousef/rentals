43f06e094a7832489602d6cd5bd18cc30400b5a5
YOUSEF <yousif0180@gmail.com>
2025-06-23 16:26:30 +0300

    some report

---
+++ rentals/rentals/report/revenue_by_maker/revenue_by_maker.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, yousef and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
