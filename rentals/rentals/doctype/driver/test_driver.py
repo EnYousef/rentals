5db65458823ceb3417b8c6a4824fecbd108079fc
YOUSEF <yousif0180@gmail.com>
2025-07-03 16:58:29 +0300

    test

--- rentals/rentals/doctype/driver/test_driver.py
+++ rentals/rentals/doctype/driver/test_driver.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2025, yousef and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestDriver(FrappeTestCase):
-	pass
+	def test_full_name_correctly_set(self):
+		test_doc = frappe.new_doc("Driver")
+		test_doc.first_name = "Yousef"
+		test_doc.first_name = "Khaled"
+		test_doc.license_number = "7824B"
+		test_doc.save()
+
+		self.assertEqual(test_doc.full_name, "Yousef Khaled")
